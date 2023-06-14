d5a6c9619667a646c9b354552788cb0e42697072
Johann <johann@helium>
2023-06-14 17:45:01 -0500

    removed old  implementations

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include "ArgumentParser.h"
 #include "ProgramOptions.h"
 
-// TODO: delete Keyboard.cpp, Keyboard.h, RawFrame, RawFrame.h and any old code;
 // TODO: get transposing working appropiatedly
 // TODO: make trackers care about their own stretegies
 // TODO: notesToRemoveFromMap from FallingNotesTracker, consider removing during the loop
 // with an iterator
+// TODO: toystory and sorry need work
 
 int main(int argc, char *argv[])
 {
